bcee6e4d1bd08da8316b74f30d2cc32ac6d55489
Bernarda Perez De Nucci <bperezde@macalester.edu>
2025-11-18 13:34:13 -0600

    started working in Journal

--- src/Journal.java
+++ src/Journal.java
@@ -1,3 +1,11 @@
-public class Journal {
+import java.util.HashMap;
 
+public class Journal {
+    private HashMap <String, Movie> allMovies;
+    private HashMap <Movie, Integer> ratings;
+
+    public Journal(){
+        allMovies = new HashMap<>();
+        ratings = new HashMap<>();
+    }
 }
