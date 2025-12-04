8ec6cfab296ef30a158af0f49de2e3717191a237
SilasRevenaugh <silasrevenaugh@gmail.com>
2025-12-03 19:11:30 -0600

    Added Rudimentary User Taste Profile, Still Need To Figure Out Weighting

--- src/Journal.java
+++ src/Journal.java
@@ -1,49 +1,60 @@
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Keeps track of all movies in the system and the movies logged by the user along with the ratings for each
+ * Keeps track of all movies in the system and the movies logged by the user along with the ratings
+ * for each
  */
 
 public class Journal {
 
     private HashMap<Movie, Integer> userMovies = new HashMap<>();
     private DataProcessor dataProcessor;
+    private Movie userTasteProfile;
 
-    
-        public Journal(DataProcessor processor){
-            this.dataProcessor = processor;
-        }
 
-        /**
-        * Checks if a movie is in the user's journal
-        */
-        public boolean hasWatched(Movie m) {
-            return userMovies.containsKey(m);
+    public Journal(DataProcessor processor) {
+        this.dataProcessor = processor;
+    }
+
+    /**
+     * Checks if a movie is in the user's journal
+     */
+    public boolean hasWatched(Movie m) {
+        return userMovies.containsKey(m);
+    }
+
+    /**
+     * Gets the rating from a movie in the user's journal
+     */
+    public int getRating(Movie m) {
+        return userMovies.getOrDefault(m, -1);
+    }
+
+    /**
+     * Returns all movies in the user's journal
+     */
+    public Collection<Movie> getWatchedMovies() {
+        return userMovies.keySet();
+    }
+
+    /**
+     * Allows user to log a movie and its rating to the user's Journal
+     */
+    public void addToUserMovies(String title, int rating) {
+        Movie movie = dataProcessor.getAllMovies().get(title);
+        if (movie != null) {
+            userMovies.put(movie, rating);
+            updateUserTasteProfile(movie);
         }
-    
-        /**
-        * Gets the rating from a movie in the user's journal
-        */
-        public int getRating(Movie m) {
-            return userMovies.getOrDefault(m, -1); 
-        }
-    
-        /**
-        * Returns all movies in the user's journal
-        */
-        public Collection<Movie> getWatchedMovies() {
-                return userMovies.keySet();
-        }
-        
-         /**
-        * Allows user to log a movie and its rating to the user's Journal
-        */
-        public void addToUserMovies(String title, int rating){
-            Movie movie = dataProcessor.getAllMovies().get(title);
-            if (movie != null) {
-                userMovies.put(movie, rating);
-            }
+    }
+
+    public void updateUserTasteProfile(Movie movie) {
+        userTasteProfile.addGenres(movie.getGenres());
+        userTasteProfile.addCast(movie.getLeadActors());
+        userTasteProfile.addKeywords(movie.getKeywords());
+        userTasteProfile.addDirector(movie.getDirector());
     }
 
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class Movie {
     private String title;
     private String[] cast;
     private String director;
+    private ArrayList<String> directorList = new ArrayList<>();;
 
     public Movie(String title, String director, String[] cast, String[] genres, String[] keywords) {
         this.title = title;
@@ -41,6 +43,10 @@ public class Movie {
         return keywords;
     }
 
+    public ArrayList<String> getDirectorList() {
+        return directorList;
+    }
+
     public void setTitle(String newTitle) {
         title = newTitle;
     }
@@ -60,6 +66,33 @@ public class Movie {
     public void setCast(String[] newCast) {
         cast = newCast;
     }
+
+    public void addGenres(String[] additionalGenres) {
+        String[] newGenres = new String[genres.length + additionalGenres.length];
+        System.arraycopy(genres, 0, newGenres, 0, genres.length);
+        System.arraycopy(additionalGenres, 0, newGenres, genres.length, additionalGenres.length);
+        genres = newGenres;
+    }
+
+    public void addKeywords(String[] additionalKeywords) {
+        String[] newKeywords = new String[keywords.length + additionalKeywords.length];
+        System.arraycopy(keywords, 0, newKeywords, 0, keywords.length);
+        System.arraycopy(additionalKeywords, 0, newKeywords, keywords.length, additionalKeywords.length);
+        keywords = newKeywords;
+    }
+
+    public void addCast(String[] additionalCast) {
+        String[] newCast = new String[cast.length + additionalCast.length];
+        System.arraycopy(cast, 0, newCast, 0, cast.length);
+        System.arraycopy(additionalCast, 0, newCast, cast.length, additionalCast.length);
+        cast = newCast;
+    }
+
+    public void addDirector(String newDirector) {
+        directorList.add(newDirector);
+    }
+
+
     
     public String toString() {
     return "Movie{" +
